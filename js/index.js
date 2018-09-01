bdf40d9129022476dcd706da2149c79b3862df4c
Paul Martin <paulmartin91@hotmail.co.uk>
2018-09-01 18:26:12 +0100

    Added coding gag

--- js/index.js
+++ js/index.js
@@ -13,6 +13,9 @@ if (navOpen == false) {
 //flips the arrow to 180 degrees
   document.getElementById("arrow").style.transform = "rotate("+ arrowRotate +"deg)";
   
+//changes the text to 'Home'
+document.getElementById('arrow').innerHTML = 'Home↓';
+  
 //sets the rotation back to 0
   arrowRotate=0;
   
@@ -26,6 +29,9 @@ if (navOpen == false) {
 //flips the arrow to 0 degrees
   document.getElementById("arrow").style.transform = "rotate("+ arrowRotate +"deg)";
   
+//changes the text back to 'Projects'
+  document.getElementById('arrow').innerHTML = '↓Projects';
+  
 //sets the rotation back to 180
   arrowRotate=180;
   
@@ -33,4 +39,62 @@ if (navOpen == false) {
   navOpen = false;
 }
 
+}
+
+/*TextTypeGag*/
+
+var text1 = [["Coding genius", 500], ["Coding Pro", 2000], ["Can write \"code\"", 2000], ["Amozinng Speller", 0], ["Self taught coder", 4000000000000000000000]]
+var i = 0;
+var j = 0;
+var pauseTime = 5000;
+
+/*Starting the process*/
+function start(arg, message) {
+
+/*Picks a random quote, assigns it to "message"*/
+var mess = text1[j];
+message = mess[0];
+var PT = mess[1];
+j++;
+/* Var for Typing out the text*/
+var startTyping = function type(arg) {
+  document.getElementById(arg.id).innerHTML += message[i];
+  }
+  console.log(message);
+  
+/*Go = Starts typing out the text, stops when gets to text length*/
+var go = setInterval(function(){
+if(i < message.length) {
+  startTyping(arg)
+  i++;
+} else {
+  
+/*Stops the process*/
+  clearInterval(go);
+  console.log("Go Complete, "+PT+" Second Pause Start");
+  setTimeout(function(){ 
+  
+    /*backspaces*/
+var back = setInterval(function(){ 
+  if (i > 0) {
+var backspaced = document.getElementById(arg.id).innerHTML;
+var backspace1 = backspaced.slice(0, backspaced.length-1);
+document.getElementById(arg.id).innerHTML= backspace1;
+    i--;
+  } else {
+  clearInterval(back);
+  console.log("Backspace Finsihed!")
+  ;
+  /*The process has now finished, start from the top!*/
+  setTimeout(function(){start(arg)}, 1000);
+  }
+}, 100);
+    
+  console.log("Pause Complete, starting backspace");}, PT);
+}
+}, 100/*delay for keystrokes*/);
+  
+/*Innitiates Go*/
+go();
+  
 }
